77b15021f689d79ebfb20b019055a97dbfa886e4
MEONJIYEON <108587227+MEONJIYEON@users.noreply.github.com>
2024-07-01 21:55:52 +0900

    html -> pdf 저장
    html -> pdf 저장

--- src/main/java/com/ls/in/approval/controller/impl/DigitalApprovalControllerImpl.java
+++ src/main/java/com/ls/in/approval/controller/impl/DigitalApprovalControllerImpl.java
@@ -2,20 +2,20 @@ package com.ls.in.approval.controller.impl;
 
 import com.ls.in.approval.controller.DigitalApprovalController;
 import com.ls.in.approval.service.DigitalApprovalService;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ls.in.approval.util.LoadHtml;
 
-import java.io.File;
-import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("/api/v1/lighting_solutions")
@@ -23,38 +23,116 @@ import java.nio.file.Paths;
 public class DigitalApprovalControllerImpl implements DigitalApprovalController {
 
     private DigitalApprovalService approvalService;
+    private final LoadHtml loadHtml = new LoadHtml();
 
     @Autowired
-    public DigitalApprovalControllerImpl(DigitalApprovalService approvalService){
+    public DigitalApprovalControllerImpl(DigitalApprovalService approvalService) {
         this.approvalService = approvalService;
     }
 
     @GetMapping("/test")
-    public String test(){
+    public String test() {
         return "test";
     }
 
-    // resources 에서 폼 가져오기
-    @GetMapping("/getHtmlContent")
-    public ResponseEntity<String> getHtmlContent() {
+    // html 양식 가져오기
+    @GetMapping("/form")
+    public ResponseEntity<String> getHtmlContent(@RequestParam Integer status) {
         String htmlContent = "";
 
-        try {
-            // Load the HTML file from the resources directory
-            File file = new File(getClass().getClassLoader().getResource("forms/draftForm.html").getFile());
-            String html = new String(Files.readAllBytes(Paths.get(file.getPath())));
-
-            // Parse the HTML file using Jsoup
-            Document document = Jsoup.parse(html);
-
-            // Extract the content inside the <body> tag
-            Element body = document.body();
-            htmlContent = body.html();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        switch (status) {
+            case 0:
+                // 기안문
+                htmlContent = loadHtml.load("forms/draftForm.html");
+                System.out.println("기안문" + status);
+                break;
+            case 1:
+                // 회의록
+                htmlContent = loadHtml.load("forms/meetingForm.html");
+                System.out.println("회의록" + status);
+                break;
+            case 2:
+                // 협조문
+                htmlContent = loadHtml.load("forms/cooperationForm.html");
+                System.out.println("협조문" + status);
+                break;
+            default:
+                // 다른 상황 처리
+                htmlContent = "Not found";
+                break;
         }
 
         return ResponseEntity.ok(htmlContent);
     }
+
+
+    // 결재 요청
+    @PostMapping("/approval/request")
+    public ResponseEntity<String> approvalRequest(@RequestBody Map<String, String> request) {
+        String status = request.get("status");
+        System.out.println(status);
+
+        // 서버에 작성한 전자결재 저장하기
+        switch (status) {
+            case "0" ->
+                // 기안문
+                    loadHtml.save(request, "src/main/resources/writeForms/draftForm.html");
+            case "1" ->
+                // 회의록
+                    loadHtml.save(request, "src/main/resources/writeForms/meetingForm.html");
+            case "2" ->
+                // 협조문
+                    loadHtml.save(request, "src/main/resources/writeForms/cooperationForm.html");
+        }
+
+
+        // html -> pdf 변경하기 (결재진행중 저장)
+
+
+
+        return ResponseEntity.ok("HTML content received and processed successfully");
+    }
+
+// 예시 메소드: 부서명을 조회하는 메소드
+    private String getDepartmentName() {
+        // 여기에 실제 데이터베이스에서 부서명을 조회하는 로직을 구현
+        return "영업부";
+    }
+
+    //예시 메소드: 이름 조회하는 메소드
+    private String getName() {
+        return "냥냥냥";
+    }
 }
+    /*
+    // 결재 요청
+    @GetMapping("/approval/request")
+    public ResponseEntity<String> getApprovalRequest() {
+        Map<String, String> data = new HashMap<>();
+        data.put("createdDate", LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd")));
+
+        try {
+            Resource resource = new ClassPathResource("forms/draftForm.html");
+            String htmlContent = new String(Files.readAllBytes(resource.getFile().toPath()));
+
+            // 데이터를 HTML에 삽입
+            for (Map.Entry<String, String> entry : data.entrySet()) {
+                htmlContent = htmlContent.replace("${" + entry.getKey() + "}", entry.getValue());
+            }
+            // 부서명 추가 로직
+            String departmentName = getDepartmentName(); // 예시: 실제 데이터베이스에서 부서명을 조회하는 로직 필요
+
+            // 이름 추가 로직
+            String name = getName();
+
+            htmlContent = htmlContent.replace("${departmentName}", departmentName);
+            htmlContent = htmlContent.replace( "${name}", name);
+            return ResponseEntity.ok(htmlContent);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("Error loading the HTML content");
+        }
+    }
+}
+
+     */
---
+++ src/main/java/com/ls/in/approval/util/LoadHtml.java
@@ -0,0 +1,59 @@
+package com.ls.in.approval.util;
+
+import lombok.NoArgsConstructor;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Map;
+
+@NoArgsConstructor
+public class LoadHtml {
+
+    public String load(String filePath){
+        String htmlContent = "";
+
+        try{
+            File file = new File(getClass().getClassLoader().getResource(filePath).getFile());
+            String html = new String(Files.readAllBytes(Paths.get(file.getPath())));
+
+            // Parse the HTML file using Jsoup
+            Document document = Jsoup.parse(html);
+
+            // Extract the content inside the <body> tag
+            Element body = document.body();
+            htmlContent = body.html();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return htmlContent;
+    }
+
+    public Map<String, String> save(Map<String, String> request, String filePath){
+        String encodedHtmlContent = request.get("html");
+        String htmlContent;
+        try {
+            htmlContent = URLDecoder.decode(encodedHtmlContent, StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return Map.of("status", "error", "message", "Failed to decode HTML.");
+        }
+
+        try {
+            Files.write(Paths.get(filePath), htmlContent.getBytes(StandardCharsets.UTF_8), StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
+            return Map.of("status", "success", "message", "HTML saved successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Map.of("status", "error", "message", "Failed to save HTML.");
+        }
+
+    }
+}
