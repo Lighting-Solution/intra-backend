eb744453442620c4aa4fa6b80adcfb8ec02ffd30
chul5 <och7164@gmail.com>
2024-07-08 11:19:07 +0900

    feat : 게시글에서 다운로드 받기 위한 기능 구현

--- src/main/java/com/ls/in/document/controller/DocumentController.java
+++ src/main/java/com/ls/in/document/controller/DocumentController.java
@@ -1,6 +1,7 @@
 package com.ls.in.document.controller;
 
 
+import com.ls.in.document.domain.model.DocumentBox;
 import com.ls.in.document.dto.DocumentDTO;
 import com.ls.in.document.dto.DocumentInitDTO;
 import com.ls.in.document.dto.DocumentList;
@@ -8,11 +9,19 @@ import com.ls.in.document.service.DocumentService;
 import com.ls.in.document.service.FileStorageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -24,7 +33,12 @@ public class DocumentController {
 	private final FileStorageService fileStorageService;
 	@PostMapping("/api/docsList")
 	public List<DocumentList> getPublicDocs(@RequestBody DocumentDTO documentDTO) {
-		documentService.getDocs(documentDTO);
+//		List<DocumentBox> docs = documentService.getDocs(documentDTO);
+
+		Pageable pageable = PageRequest.of(documentDTO.getPage(), documentDTO.getSize());
+		List<DocumentBox> docs = documentService.getDocs(documentDTO, pageable);
+		log.info("documentDTO={}", documentDTO);
+		log.info("docs:{}", docs.toString());
 		return null;
 	}
 
@@ -35,12 +49,32 @@ public class DocumentController {
 			@RequestParam("file") MultipartFile file,
 			@RequestParam("category") String category,
 			@RequestParam("writerEmpId") Integer writerEmpId) {
-
 		String fileName = fileStorageService.storeFile(file);
-
 		DocumentInitDTO document = new DocumentInitDTO(title, content, fileName, category, writerEmpId);
 		documentService.saveDocument(document);
-
 		return new ResponseEntity<>("Document created successfully", HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}/download")
+	public ResponseEntity<Resource> downloadFile(@PathVariable Integer id) {
+		// Document 엔티티에서 파일 이름을 가져오는 로직을 추가해야 합니다.
+		DocumentBox document = documentService.getDocumentById(id);
+		String fileName = document.getDocumentPath();
+
+		try {
+			Path filePath = Paths.get("src/main/resources/docs").resolve(fileName).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+
+			if (resource.exists()) {
+				return ResponseEntity.ok()
+						.contentType(org.springframework.http.MediaType.APPLICATION_OCTET_STREAM)
+						.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+						.body(resource);
+			} else {
+				return ResponseEntity.notFound().build();
+			}
+		} catch (Exception e) {
+			return ResponseEntity.internalServerError().build();
+		}
 	}
 }
