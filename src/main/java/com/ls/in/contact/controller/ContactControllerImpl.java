5573e22f83ce9bc2561596827903221848bb9d42
minseong-Kang <80028813+minseong00@users.noreply.github.com>
2024-07-10 21:55:00 +0900

    API 수정

--- src/main/java/com/ls/in/contact/controller/ContactControllerImpl.java
+++ src/main/java/com/ls/in/contact/controller/ContactControllerImpl.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/intranet/contact")
+@RequestMapping("/api/v1/lighting_solutions/contact")
 @CrossOrigin(origins = "http://localhost:3000")
 public class ContactControllerImpl implements ContactController {
 
