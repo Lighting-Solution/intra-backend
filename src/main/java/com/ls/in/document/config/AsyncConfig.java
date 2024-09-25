b32a5c6dc30e76613862a09c6ca38c5e94190978
chul5 <och7164@gmail.com>
2024-09-25 21:02:34 +0900

    chore: 주석 설명

--- src/main/java/com/ls/in/document/config/AsyncConfig.java
+++ src/main/java/com/ls/in/document/config/AsyncConfig.java
@@ -11,6 +11,13 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 @EnableAsync
+
+/**
+ * 	•	설정된 ThreadPoolTaskExecutor가 스레드를 관리하며, 비동기 작업이 발생하면 스레드 풀에서 스레드를 할당합니다.
+ * 	•	CorePoolSize는 스레드 풀에서 기본적으로 유지하는 스레드 수(5개).
+ * 	•	MaxPoolSize는 필요할 경우 최대 허용되는 스레드 수(10개).
+ * 	•	QueueCapacity는 스레드가 부족할 때 작업이 대기하는 큐의 크기(25개).
+ */
 public class AsyncConfig {
 	@Bean(name = "taskExecutor")
 	public Executor asyncExecutor(){
